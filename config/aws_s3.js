a00d1a5e661f381ad83bbf071b08e0e0fb2ed878
Evan J. Washington <enavy04@gmail.com>
2017-06-26 22:21:24 -0700

    updated aws config file.

--- config/aws_s3.js
+++ config/aws_s3.js
@@ -1,29 +1,31 @@
 const aws = require('aws-sdk');
 
-// module to export images to s3 bucket
-// parameters require a path, a key(or file name), and a fileType
-module.exports = function(path, key, fileType){
-	const S3_BUCKET = process.env.S3_BUCKET;
-	const s3 = new aws.S3();
-	const fileName = `${path}/${key}`;
-	const s3Params = {
-	Bucket: S3_BUCKET,
-	Key: fileName,
-	Expires: 60,
-	ContentType: fileType,
-	ACL: 'public-read'
-	};
-	s3.getSignedUrl('putObject', s3Params, (err, data) => {
-	if(err){
-	  console.log(err);
-	  return res.end();
-	}
-	const returnData = {
-	  signedRequest: data,
-	  url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
-	};
-	console.log(returnData);
-	res.write(JSON.stringify(returnData));
-	res.end();
-	});	
+// module to get a signed url from amazon server.
+// parameters require a path, a name for the file, and a fileType
+
+module.exports =function (path, name, fileType, req, res){
+/*
+ * Load the S3 information from the environment variables.
+ */
+  const S3_BUCKET = process.env.S3_BUCKET;
+  const s3 = new aws.S3();
+  const fileName = `${path}/${name}`;
+  const s3Params = {
+  Bucket: S3_BUCKET,
+  Key: fileName,
+  Expires: 60,
+  ContentType: fileType,
+  ACL: 'public-read'
+  };
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+  if(err){
+    console.log(err);
+    res.send(err);
+  }
+  const returnData = {
+    signedRequest: data,
+    url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+  };
+  res.json(returnData);
+  }); 
 };
