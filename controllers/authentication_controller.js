9f9059361b4040ee5c107d9ebdb1b72d09315ade
Evan J. Washington <enavy04@gmail.com>
2017-06-29 16:48:16 -0700

    merged auth controller with tokens controller

--- controllers/authentication_controller.js
+++ controllers/authentication_controller.js
@@ -1,42 +1,47 @@
 const User = require('../models/user');
-const token = require('./tokens_controller');
-const controller = {};
+const jwt = require('jsonwebtoken');
 const EMAIL_IN_USE='Email is in use!';
 const MISSING_PARAMETERS='One or more parameters are missing!';
+const JWT_SECRET = process.env.PRAYER_HUB_JWT_SECRET;
+const controller = {};
 
+// Function for signing the user generated token
+const sign = (user) => {
+	const token = jwt.sign(
+	    {
+	      sub: user._id,
+	      role: user.role
+	    },
+	    JWT_SECRET,
+	    {
+	    	algorithm: 'HS512'
+	    }
+	 );
+	return ({token});
+};
+
+//creates a new user
 controller.signup = (req,res, next) => {
-	//creates a new user
-	const dataUser = req.body;
-	const email = dataUser.email;
-	const userName = dataUser.userName;
-	const password = dataUser.password;
-	const location = dataUser.location;
-	const name = dataUser.name;
-	const role = dataUser.role;
 	// If an important parameter is missing, throw an error.
-	// Check to see if email is already in use.
+	const {email, userName, password, location, name, role} = req.body;
 	User.findByEmail(email).select('email -_id')
 	.then((user) => {
+		
+		// Check to see if email is already in use.
 		if(user) throw Error (EMAIL_IN_USE);
+		// Check to make sure all parameters are present
 		if(!email || !userName || !password || !location || !name) throw Error(MISSING_PARAMETERS);
 	})
 	.then(() =>{
 
-		const newUser = new User(
-		{
-			email,
-			password,
-			role,
-			userName,
-			name,
-			location,
-			lastSignIn: new Date
-		});
+		const newUser = new User()
+		Object.assign(newUser, req.body);
+		newUser.lastSignIn = new Date;
 		return newUser.save()
 	})
 	.then((user) => {
 		//if create was successful
-		res.status(200).send(token.sign(user));
+		res.status(200).send(sign(user));
 	})
 	.catch((err) => {
 		//error handling
@@ -45,13 +50,13 @@ controller.signup = (req,res, next) => {
 	});
 
 };
+// Sign in existing user by generating a new token
 controller.signin = (req,res,next) => {
-	res.status(200).send(token.sign(req.user));
+	res.status(200).send(sign(req.user));
 };
 
-
+// Shows the signed in User
 controller.signedUser = (req,res) => {
-	console.log(req.user);
 	res.send(req.user);
 };
 
